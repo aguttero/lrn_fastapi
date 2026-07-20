fcf4e9a3cbdde2aa23a6b11a240605f71915cef2
alejandro guttero <aguttero@gmail.com>
2026-07-20 09:02:56 -0400

    0719 oop review!

--- src_tmp/OOP/Enemy.py
+++ src_tmp/OOP/Enemy.py
@@ -8,21 +8,26 @@ class Enemy:
     #     pass
 
     def __init__(self, type_of_enemy, health_points = 10, attack_damage = 1):
-       self._type_of_enemy = type_of_enemy
+       self.__type_of_enemy = type_of_enemy # double underscore -> Private Encapsulation
        self.health_points = health_points
        self.attack_damage = attack_damage
-       print(f'New {self._type_of_enemy} enemy instance created')
+       print(f'New {self.__type_of_enemy} enemy instance created')
 
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
+
+    def set_type_of_enemy(self, type_of_enemy):
+        self.__type_of_enemy = type_of_enemy
 
     def talk(self):
-        print(f"I am a {self._type_of_enemy}. Be prepared to fight!")
+        print(f"I am a {self.__type_of_enemy}. Be prepared to fight!")
         return "done!"
 
     def walk_forward(self):
-        print(f"{self._type_of_enemy} moves closer to you")
+        print(f"{self.__type_of_enemy} moves closer to you")
 
     def attack(self):
-        print(f"{self._type_of_enemy} attacks for {self.attack_damage} damage")
+        print(f"{self.__type_of_enemy} attacks for {self.attack_damage} damage")
 
     def health_check(self):
         print(f"{self.health_points} health points remaining")
--- src_tmp/oop_1.py
+++ src_tmp/oop_1.py
@@ -1,9 +1,19 @@
 from OOP.Enemy import *
 
-zombie = Enemy('Zombie')
+zombie = Enemy('Zombie', 100, 3)
 # zombie.type_of_enemy = 'Zombie'
 
-print(f'{zombie._type_of_enemy} has {zombie.health_points} health points and can do attack of {zombie.attack_damage}')
+# print(f'{zombie.__type_of_enemy} has {zombie.health_points} health points and can do attack of {zombie.attack_damage} damage')
+
+print ("- - -")
+print(f'{zombie.get_type_of_enemy()} has {zombie.health_points} health points and can do attack of {zombie.attack_damage} damage')
+
+# --- This is the way to 'override' the private attribute
+zombie._Enemy__type_of_enemy = "Vampire"
+
+print ("- - -")
+print(f'{zombie.get_type_of_enemy()} has {zombie.health_points} health points and can do attack of {zombie.attack_damage} damage')
+
 
 # zombie.talk()
 # zombie.walk_forward()
